df21ee037ed756bbfee7313dad2fdfe8edb3cbad
maskrenhas2022 <maskrenhas02@.gmail.com>
2025-07-03 22:14:06 -0400

    updates

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit{
   protected title = 'gerador-certificado';
+
+  exibeNavbar:boolean = true;
+  ngOnInit(): void {
+   this.mensagem();
+  }
+    mensagem(){
+      console.log('Start Components now (Inside in the function)')
+    }
 }
